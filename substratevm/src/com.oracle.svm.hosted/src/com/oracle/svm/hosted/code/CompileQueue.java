3b0d264097398bde3fdcbfcbe419ada832cecb56
David Kozak <david.kozak@oracle.com>
2022-03-07 11:59:37 +0100

    more cleanup

--- substratevm/src/com.oracle.svm.hosted/src/com/oracle/svm/hosted/code/CompileQueue.java
+++ substratevm/src/com.oracle.svm.hosted/src/com/oracle/svm/hosted/code/CompileQueue.java
@@ -1098,7 +1098,6 @@ public class CompileQueue {
                 graph = new StructuredGraph.Builder(getCustomizedOptions(debug), debug).method(method).build();
             }
         }
-        method.compilationInfo.graph = graph;
         try (DebugContext.Scope s = debug.scope("Parsing", graph, method, this)) {
 
             try {
